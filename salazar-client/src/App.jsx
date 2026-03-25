856000a449f0b1e45f315582ffb8ae34efeef70a
Your Name <your.email@example.com>
2026-03-25 19:46:26 +0800

    lab-act-instance

---
+++ salazar-client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Salazar, Cheska Ann Lorein Jose<br />
+          Email: cheskasalazar11@gmail.com<br />
+          Other Personal Info: Bachelor of Science in Information Technology
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
